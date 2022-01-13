61c45169151cb2748de7a5b83656aa51352067fb
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-13 04:41:45 +0000

    KE-0001-Methods: Passing parameters

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,16 +10,41 @@ namespace Methods
     {
 
         // We first make our method and call it myname and then in static void main the method myname is called. This is a simple method there is no return type or any arguments to pass.
+
+        /*
         static void Main(string[] args)
         {
             myname();
         }
-
         static void myname()
         {
-            Console.WriteLine("Hello World!");
+           Console.WriteLine("Hello World!");
+           Console.ReadLine();
+        }
+        */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter product cost: ");
+            string pC = Console.ReadLine();
+
+            Console.WriteLine("Now enter current VAT rate: ");
+            string cR = Console.ReadLine();
+
+            var result = VAT(Convert.ToDouble(pC), Convert.ToDouble(cR));
+
+            Console.WriteLine("Total cost is {0:C}", result);
             Console.ReadLine();
         }
+
+        // Passing Parameters
+
+        static double VAT(double productCost, double currentRate)
+        {
+            double CR = (currentRate + 100) / 100;
+            return productCost * CR;
+        }
+
 
     }
 }
