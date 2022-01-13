178b0ad1a4028439a37d28cbd092e747c8091d51
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-13 04:31:24 +0000

    KE-0001-Methods: variables in Methods, creating an example

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -8,8 +8,18 @@ namespace Methods
 {
     internal class Program
     {
+
+        // We first make our method and call it myname and then in static void main the method myname is called. This is a simple method there is no return type or any arguments to pass.
         static void Main(string[] args)
         {
+            myname();
         }
+
+        static void myname()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
     }
 }
